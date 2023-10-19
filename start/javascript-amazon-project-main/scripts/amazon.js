6a87ec253c17a030451bff0d165131bdd620dd39
Abhimanyu <abhimanyu3398@gmail.com>
2023-10-19 19:57:26 +0530

    update cartquantity on homepage

--- start/javascript-amazon-project-main/scripts/amazon.js
+++ start/javascript-amazon-project-main/scripts/amazon.js
@@ -69,9 +69,15 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
                 productId:productId,
                 quantity:1
             });
-        }
+        };
+        let cartQuantity=0;
+        cart.forEach((item)=>{
+          cartQuantity+=item.quantity;
+        });
+
+        document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
 
         
-        console.log(cart);
+        console.log(cartQuantity);
     });
 });
